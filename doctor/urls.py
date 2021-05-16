7e509f347db8dbb2ca4e27749a00357b47c9433c
tttccc112 <18377152@buaa.edu.cn>
2021-05-16 20:56:39 +0800

    5.16
    部分更新

--- doctor/urls.py
+++ doctor/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
     # url(r'^index/(\wt)/(\wt)', views.index),
     # url(r'^choose/', views.choose.as_view()),
     url(r'^index/(\w+)/(\w+)', views.index.as_view()),
-    url(r'^history/(\w+)/(\w+)', views.history),
-    url(r'^check/(\w+)/(\w+)', views.check),
-    url(r'^decide/(\w+)/(\w+)', views.decide),
+    url(r'^history/(\w+)/(\w+)', views.history.as_view()),
+    url(r'^check/(\w+)/(\w+)', views.check.as_view()),
+    url(r'^decide/(\w+)/(\w+)', views.decide.as_view()),
     url(r'^arrange/(\w+)/(\w+)', views.arrange),
     url(r'^evaluation/(\w+)/(\w+)', views.evaluation),
 
--- doctor/views.py
+++ doctor/views.py
@@ -32,10 +32,6 @@ class index(View):
             #根据所得简写检索得到药品列表[id，名称]，改写message后发送给前端
             ret['message']=[['0','好吃药'],['1','难吃药']]
             return HttpResponse(json.dumps(ret))
-        elif entrance=='add_medi':
-            #将新增的药品加入其中
-            add_medicines=request.POST.getlist('add_medicines')
-            return HttpResponse(json.dumps(ret))
         elif entrance=='save_discription':
             #更新诊断数据库
             text=request.POST.get('text')
@@ -46,14 +42,53 @@ class index(View):
             ret['message']=['尿检','2000-1-12',['DNA','RNA'],['1','0.9']]
             return HttpResponse(json.dumps(ret))
 
-def history(request,doc,pat):
-    return render(request,'history.html')
 
-def check(request,doc,pat):
-    return render(request,'check.html')
+class history(View):
+    def get(self,request,doc,pat):
 
-def decide(request,doc,pat):
-    return render(request,'decide.html')
+        return render(request,'history.html')
+
+    def post(self,request,doc,pat):
+        entrance=request.POST.get('entrance')
+        ret={'status':True,'message':None}
+        if entrance=='his':
+            return HttpResponse(json.dumps(ret))
+        elif entrance=='search_check':
+            #提供该检查id下的详情
+            id=request.POST.get('id')
+            ret['message']=['尿检','2000-1-12',['DNA','RNA'],['1','0.9']]
+            return HttpResponse(json.dumps(ret))
+
+class check(View):
+    def get(self,request,doc,pat):
+
+        return render(request,'check.html')
+
+    def post(self,request,doc,pat):
+        entrance=request.POST.get('entrance')
+        ret={'status':True,'message':None}
+        if entrance=='ch':
+            return HttpResponse(json.dumps(ret))
+        elif entrance=='search_check':
+            #提供该检查id下的详情
+            id=request.POST.get('id')
+            ret['message']=['尿检','2000-1-12',['DNA','RNA'],['1','0.9']]
+            return HttpResponse(json.dumps(ret))
+
+
+class decide(View):
+    def get(self,request,doc,pat):
+
+        return render(request,'decide.html')
+
+    def post(self,request,doc,pat):
+        entrance=request.POST.get('entrance')
+        ret={'status':True,'message':None}
+        if entrance=='result':
+            #返回诊断结果
+            ret['message']='急性心梗'
+            return HttpResponse(json.dumps(ret))
+
 
 def arrange(request,doc,pat):
     return render(request,'arrange.html')
